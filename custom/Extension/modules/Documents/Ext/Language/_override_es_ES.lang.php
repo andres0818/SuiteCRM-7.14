3ef9099576338aecc2631594d95aafd0782de79f
andres0818 <c.andres.perez.h@gmail.com>
2023-09-15 09:16:15 -0500

    Se cambio el nombre de un campo

--- custom/Extension/modules/Documents/Ext/Language/_override_es_ES.lang.php
+++ custom/Extension/modules/Documents/Ext/Language/_override_es_ES.lang.php
@@ -1,4 +1,5 @@
 <?php
-// created: 2023-09-15 15:42:02
+// created: 2023-09-15 16:13:18
 $mod_strings['LBL_DOCUMENT_INFORMATION'] = 'Creación Documentos Controlados.';
 $mod_strings['LBL_EMAIL'] = 'Correo electrónico';
+$mod_strings['LBL_NAME'] = 'Nombre de Doc';
