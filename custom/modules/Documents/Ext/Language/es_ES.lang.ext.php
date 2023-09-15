fb0c7a17334a3bf72af11482fd9e08f703089175
andres0818 <c.andres.perez.h@gmail.com>
2023-09-15 09:36:32 -0500

    Se elimina un campo

--- custom/modules/Documents/Ext/Language/es_ES.lang.ext.php
+++ custom/modules/Documents/Ext/Language/es_ES.lang.ext.php
@@ -2,7 +2,8 @@
 // WARNING: The contents of this file are auto-generated
 
 
-// created: 2023-09-15 15:42:02
+// created: 2023-09-15 16:16:25
 $mod_strings['LBL_DOCUMENT_INFORMATION'] = 'Creación Documentos Controlados.';
 $mod_strings['LBL_EMAIL'] = 'Correo electrónico';
+$mod_strings['LBL_NAME'] = 'Nombre de Documento';
 
