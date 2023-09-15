ecf971fc795f8955eb3c3c174d4cd2a65c2f0639
andres0818 <c.andres.perez.h@gmail.com>
2023-09-15 09:17:01 -0500

    Se cambio el nombre de un campo de nuevo para confirmar el archivo

--- custom/Extension/modules/Documents/Ext/Language/_override_es_ES.lang.php
+++ custom/Extension/modules/Documents/Ext/Language/_override_es_ES.lang.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2023-09-15 16:13:18
+// created: 2023-09-15 16:16:25
 $mod_strings['LBL_DOCUMENT_INFORMATION'] = 'Creación Documentos Controlados.';
 $mod_strings['LBL_EMAIL'] = 'Correo electrónico';
-$mod_strings['LBL_NAME'] = 'Nombre de Doc';
+$mod_strings['LBL_NAME'] = 'Nombre de Documento';
