7879e8105df2ba986cda5c5f08b25c1b536cd4e9
andres0818 <c.andres.perez.h@gmail.com>
2023-09-15 08:37:08 -0500

    busqueda no encontrada

---
+++ custom/Extension/modules/Documents/Ext/Language/_override_es_ES.lang.php
@@ -0,0 +1,3 @@
+<?php
+// created: 2023-09-15 15:35:20
+$mod_strings['LBL_DOCUMENT_INFORMATION'] = 'Creación Documentos Controlados.';
---
+++ custom/modules/Documents/Ext/Language/es_ES.lang.ext.php
@@ -0,0 +1,7 @@
+<?php
+// WARNING: The contents of this file are auto-generated
+
+
+// created: 2023-09-15 15:35:20
+$mod_strings['LBL_DOCUMENT_INFORMATION'] = 'Creación Documentos Controlados.';
+
